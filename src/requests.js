62b85ea4855005b47c35e31fe8d415d598c5ed97
Joseph Dubon <josephdubon@pm.me>
2020-10-25 16:29:35 -0700

    Removed top trending, was not working.

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,6 @@
 const API_KEY = '66fc13ab589d1d905e3a3546e4bbc9f2'
 
 const requests = {
-  fetchTrending: `/trending/all/week/?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en_US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
